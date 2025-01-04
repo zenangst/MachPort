cfae60052a90b1a6ccd6d6747b9353a4eebfa408
Christoffer Winterkvist <christoffer@winterkvist.com>
2025-01-04 18:04:41 +0100

    Update key shortcut extensions and tests to cover more cases

--- Sources/MachPort/MachPortEventController.swift
+++ Sources/MachPort/MachPortEventController.swift
@@ -141,7 +141,7 @@ public final class MachPortEventController: MachPortEventPublisher, @unchecked S
                    configure: (CGEvent) -> Void = { _ in }) throws -> CGEvent {
     let cgEvent = try createEvent(key, type: type, flags: flags,
                                   tapLocation: tapLocation, configure: configure)
-
+    cgEvent.setIntegerValueField(.eventSourceUserData, value: signature)
     cgEvent.post(tap: tapLocation)
 
     return cgEvent
